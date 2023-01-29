b19d89d90c832de5061b0a3fd3cbb78c2bffb744
Theodoor Bongers <email@theodoorbongers.nl>
2023-01-29 19:42:54 +0100

    test

--- dc.lua
+++ dc.lua
@@ -2,7 +2,7 @@ local instruments = { "basedrum", "hat", "snare", "cow_bell" }
 
 monitor = periphals.find("monitor")
 
-function drawGrid
+function drawGrid()
   for index, name in ipairs(instruments) do
     monitor.setCursorPos(1, index)
     monitor.write(name)
