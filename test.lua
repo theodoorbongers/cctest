1c429ba6226354caa0cf8b0c19e70927b5e75b12
Theodoor Bongers <email@theodoorbongers.nl>
2023-01-29 12:29:18 +0100

    test

--- test.lua
+++ test.lua
@@ -1,1 +1,10 @@
-print("hello world!")
+local speaker = peripheral.find("speaker")
+
+local buffer = {}
+local t, dt = 0, 2 * math.pi * 220 / 48000
+for i = 1, 128 * 1024 do
+    buffer[i] = math.floor(math.sin(t) * 127)
+    t = (t + dt) % (math.pi * 2)
+end
+
+speaker.playAudio(buffer)
