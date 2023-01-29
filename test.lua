91c4ea68471dd578eb12f1f90de15e9295893439
Theodoor Bongers <email@theodoorbongers.nl>
2023-01-29 14:38:57 +0100

    test

--- test.lua
+++ test.lua
@@ -1,6 +1,18 @@
+function Set (list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+
 local stop = false
 while not stop do
+  local ignoredEvents = Set {"key_up"}
   local event = {os.pullEvent()}
   print(table.unpack(event))
-  stop = true
+
+  local eventName = table.unpack(event)
+  if not ignoredEvents[eventName]
+  then
+    stop = true
+  end
 end
