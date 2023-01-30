5deea4bd363dd4cb009b2f1fe7658e2ee1f85a50
Theodoor Bongers <email@theodoorbongers.nl>
2023-01-30 21:11:49 +0100

    test

--- dc.lua
+++ dc.lua
@@ -142,7 +142,7 @@ function processBeat()
   local previousBeatIndex = currentBeatIndex
   currentBeatIndex = currentBeatIndex % TOTAL_BEATS + 1
   for instrument, buttons in pairs(triggerButtons) do
-    print("processBeat A")
+    print("processBeat A", instrument)
     updateButtonColor(instrument, previousBeatIndex)
     updateButtonColor(instrument, currentBeatIndex)
   end
